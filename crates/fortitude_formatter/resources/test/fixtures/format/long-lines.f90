e02ef354bc44e49063e4295283df2693a07369c6
Søren Hartmann <soh@ticra.com>
2025-08-15 13:11:14 +0200

    Add split examples

--- crates/fortitude_formatter/resources/test/fixtures/format/long-lines.f90
+++ crates/fortitude_formatter/resources/test/fixtures/format/long-lines.f90
@@ -14,8 +14,22 @@ contains
 
       call some_other(abc, 1 + 2 / 2, abc)
 
-      variable = foo + bar * (thing + (jaff + zang) - yeet) + huff
+      variable = foo + bar * (thing + (jaff + zang) - f(3, 1234, abc) * yeet) + g(huff + bar * (thing + (jaff + zang) - yeet) + (huff + bar * (thing + (jaff + zang) - yeet) + huff + bar * (thing + (jaff + zang) - yeet) + huff)) * 10
 
+      call some_other( 
+         1234, abc, & ! A comment about second arg
+         1 + 2 / 2, & ! Another comment out last arg
+      )
+
+      valid = valid &
+         .and. sum(A%values()) &
+            + sum(B%values()) &
+            + sum(S%values()) == sum(node_ids%values())
+
+      valid = sum(A%values()) &
+         + sum(B%values()) &
+         + sum(S%values()) == sum(node_ids%values()) &
+         .and. is_valid
 
       a = 1 + 2 / 3
       a = 1 / 2 + 3
